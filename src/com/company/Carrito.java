8a9afd9c70e272dfefaa398db4df9e682765d7d5
AlbertoGV <agarciav88@elpuig.xeill.net>
2018-11-11 17:48:24 +0100

    p

--- src/com/company/Carrito.java
+++ src/com/company/Carrito.java
@@ -12,8 +12,7 @@ import java.util.stream.Stream;
 public class Carrito {
 
 
-
-    public static void juntarListas(List<Electronica> listaElectronicos, List<Alimentacion> listaAlimentos, List<Textil> listaTextil){
+    public static void juntarListas(List<Electronica> listaElectronicos, List<Alimentacion> listaAlimentos, List<Textil> listaTextil) {
 
         List<Object> newList = Stream.concat(listaAlimentos.stream(), listaTextil.stream())
                 .collect(Collectors.toList());
@@ -22,38 +21,92 @@ public class Carrito {
     }
 
 
-
     public static void mostrarCarro(List<Electronica> listaElectronicos, List<Alimentacion> listaAlimentos, List<Textil> listaTextil) {
 
         int cantidad = 1;
-        int precioTotal=0;
+
+        double precioTotal = 0;
+        double precioTotalAlimentos=0;
+        double precioTotalElectronica=0;
+        double precioTotalTextil=0;
 
 
         System.out.println("--------Carro de la compra-----------");
 
         System.out.println("CANTIDAD  PRODUCTO    PRECIO");
-        for (int i = 0; i < listaAlimentos.size(); i++) {
-            System.out.println(cantidad + "    X      " + listaAlimentos.get(i).nombre + "      " + listaAlimentos.get(i).precio);
+        System.out.println("*******************************");
 
-            for (int j = 0; j < listaElectronicos.size(); j++) {
-                System.out.println(cantidad + "    X      " + listaElectronicos.get(j).nombre + "      " + listaElectronicos.get(j).precio);
+        generarListaAlimentos(listaAlimentos, precioTotalAlimentos);
+        for (int i = 0; i <listaAlimentos.size() ; i++) {
+            precioTotalAlimentos= precioTotalAlimentos + listaAlimentos.get(i).getPrecio();
+        }
+        System.out.println("Precio Total de Alimentos: " + precioTotalAlimentos);
+        System.out.println("*********************************");
+        generarListaElectronicos(listaElectronicos, precioTotalElectronica);
+        for (int i = 0; i <listaElectronicos.size() ; i++) {
+            precioTotalElectronica= precioTotalElectronica + listaElectronicos.get(i).getPrecio();
+        }
+        System.out.println("Precio Total de Electronicos: "+ precioTotalElectronica);
+        System.out.println("*********************************");
 
-                for (int k = 0; k < listaTextil.size(); k++) {
-                    System.out.println(cantidad + "    X      " + listaTextil.get(k).nombre + "      " + listaTextil.get(k).precio);
+        generarListaTextiles(listaTextil, precioTotalTextil);
+        for (int i = 0; i <listaTextil.size() ; i++) {
+            precioTotalTextil= precioTotalTextil + listaTextil.get(i).getPrecio();
+        }
+        System.out.println("Precio Total de Textil: "+ precioTotalTextil);
+        System.out.println("*********************************");
+        System.out.println("--------------------------------");
+
+        precioTotal = precioTotalAlimentos + precioTotalElectronica + precioTotalTextil;
+
+        System.out.println("PRECIO TOTAL DEL CARRITO: " + precioTotal);
 
 
-                    precioTotal = (int) (listaAlimentos.get(i).precio +listaElectronicos.get(j).precio+listaTextil.get(k).precio);
-                    System.out.println("Total Productos: " + cantidad + "     " + "Precio Total:" + precioTotal);
-                    System.out.println("-------------------------------------");
+
+    }
 
 
-                }
+    public static void generarListaAlimentos(List<Alimentacion> listaAlimentos, double precioTotalAlimentos) {
+        int cantidad = 1;
+        // double precioTotalAlimentos=0;
+
+        if (listaAlimentos.isEmpty()) {
+            System.out.println("No hay Alimentos");
+        } else {
+
+            for (int i = 0; i < listaAlimentos.size(); i++) {
+                System.out.println(cantidad + "    X      " + listaAlimentos.get(i).nombre + "      " + listaAlimentos.get(i).precio);
+                precioTotalAlimentos = precioTotalAlimentos + listaAlimentos.get(i).precio;
             }
         }
 
     }
 
+    public static void generarListaElectronicos(List<Electronica> listaElectronicos, double precioTotalElectronicos) {
+        int cantidad = 1;
+        if (listaElectronicos.isEmpty()) {
+            System.out.println("No hay productos Electrónicos");
+        } else {
 
+            for (int i = 0; i < listaElectronicos.size(); i++) {
+                System.out.println(cantidad + "    X      " + listaElectronicos.get(i).nombre + "      " + listaElectronicos.get(i).precio);
+                precioTotalElectronicos = precioTotalElectronicos + listaElectronicos.get(i).precio;
+            }
+        }
+    }
+    public static void generarListaTextiles(List<Textil> listaTextiles, double precioTotalTextiles) {
+        int cantidad = 1;
+
+        if (listaTextiles.isEmpty()) {
+            System.out.println("No hay productos Textiles");
+            precioTotalTextiles = 0;
+        } else {
+
+            for (int i = 0; i < listaTextiles.size(); i++) {
+                System.out.println(cantidad + "    X      " + listaTextiles.get(i).nombre + "      " + listaTextiles.get(i).precio);
+                precioTotalTextiles = precioTotalTextiles + listaTextiles.get(i).precio;
+            }
+        }
+    }
 
 }
-
